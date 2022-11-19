bbdaed5c566fed59777981efc11565a22e49cfa6
Tribe <cindywidinda@gmail.com>
2022-11-19 17:03:11 +0700

    nomer3

--- seleksi_nilai.js
+++ seleksi_nilai.js
@@ -1,8 +1,21 @@
-let nilai = [3, 28, 8, 2, 19, 23, 9, 16, 32]
-nilai = nilai.sort((a, b) => a - b);
-const Awal = 5;
-const Akhir = 25;
-const hasil = nilai.filter(el => el > Awal && el < Akhir)
-console.log(hasil);
+function deret(awal, akhir){
+    let nilai = [3, 28, 8, 2, 19, 23, 9, 16, 32]
+    nilai = nilai.sort((a, b) => a - b);
+    terbesar = nilai[nilai.length - 1];
 
+    //kondisi yg benar
+    if(awal < akhir && terbesar > awal){
+        const hasil = nilai.filter(el => el > awal && el < akhir)
+        return hasil
+    }
+    //jika nilai lebih besar dari nilai pada array maka:
+    else if(terbesar <= awal){
+        return 'nilai tidak ditemukan'    
+    }        
 
+    //kondisi saat nilai akhir lebih besar
+    else if (awal > akhir){
+        return 'nilai akhir harus lebih besar dari nilai awal'
+    }
+}
+console.log(deret(5, 25));
